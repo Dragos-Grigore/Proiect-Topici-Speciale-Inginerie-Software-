ce1151391d327a4c7d3f201051facb71442c0a43
SabinZibileanu <s_zibileanu@yahoo.com>
2024-12-04 19:20:13 +0200

    Solved PR and added a information section on the website

--- tsis/proiect/urls.py
+++ tsis/proiect/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('run/', views.mutlimodal_page, name='multimodal_page'),
-    path('upload/', views.upload_data, name='upload_data'),
+    path('run/', views.multimodal_page, name='multimodal_page'),
+    path('upload/', views.upload_image, name='upload_image')
 ]
--- tsis/proiect/views.py
+++ tsis/proiect/views.py
@@ -2,35 +2,48 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.core.files.storage import FileSystemStorage
 
-def mutlimodal_page(request):
-    
+from lens import Lens, LensProcessor
+from PIL import Image
+import requests
+import torch
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+def multimodal_page(request):
     return render(request, 'proiect.html')
 
-def upload_data(request):
-    
-    if request.method == 'POST':
-        selected_prompt = request.POST.get("prompt-select")  
-        selected_llm = request.POST.get("llm-select")  
-        image = request.FILES.get('image')
+def upload_image(request):
+    if request.method == 'POST' and request.FILES.get('image'):
+        image = request.FILES['image']
+        fs = FileSystemStorage()
         
-        if image:
-            fs = FileSystemStorage()
-            filename = fs.save(image.name, image)
-            uploaded_image_url = fs.url(filename)
-            absolute_image_path = fs.path(filename)
-            
-            
-            return JsonResponse({
-                'message': 'Image uploaded successfully!',
-                'image_url': uploaded_image_url,
-                'image_path': absolute_image_path,
-                'selected_llm': selected_llm,
-                'selected_prompt': selected_prompt,
-            })
+        filename = fs.save(image.name, image)
 
+        message = analyze_image(image)
+        uploaded_image_url = fs.url(filename)
+        absolute_image_path = fs.path(filename)
+
+        return JsonResponse({
+            'message': message,
+            'image_url': uploaded_image_url,
+            'image_path': absolute_image_path,
+        })
     
     return JsonResponse({'message': 'Invalid request.'}, status=400)
 
+def analyze_image(image):
+    image = Image.open(image).convert('RGB')
+    question = "What is the image about?"
 
+    lens = Lens()
+    processor = LensProcessor()
+    with torch.no_grad():
+        samples = processor([image],[question])
+        lens(samples)
 
+    tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small",truncation_side = 'left',padding = True)
+    LLM_model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
 
+    input_ids = tokenizer(samples["prompts"], return_tensors="pt").input_ids
+    outputs = LLM_model.generate(input_ids)
+
+    return tokenizer.decode(outputs[0])

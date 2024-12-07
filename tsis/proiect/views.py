f5f2fa0eb16575fb412f2fb2e397706e511dbae0
Alexandru Sasu <sasualex1@gmail.com>
2024-12-07 21:18:59 +0200

    Added more LLMs

--- tsis/proiect/views.py
+++ tsis/proiect/views.py
@@ -1,49 +1,62 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.core.files.storage import FileSystemStorage
 
 from lens import Lens, LensProcessor
 from PIL import Image
-import requests
 import torch
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
 
 def multimodal_page(request):
     return render(request, 'proiect.html')
 
 def upload_image(request):
-    if request.method == 'POST' and request.FILES.get('image'):
+    if request.method == 'POST' and request.FILES.get('image') and request.POST.get('selected_llm') and request.POST.get('llm_query'):
         image = request.FILES['image']
-        fs = FileSystemStorage()
-        
-        filename = fs.save(image.name, image)
+        selected_llm = request.POST.get('selected_llm')
+        llm_query = request.POST.get('llm_query')
 
-        message = analyze_image(image)
-        uploaded_image_url = fs.url(filename)
-        absolute_image_path = fs.path(filename)
-
+        answer = analyze_image(image, selected_llm, llm_query)
         return JsonResponse({
-            'message': message,
-            'image_url': uploaded_image_url,
-            'image_path': absolute_image_path,
+            'answer': answer
         })
     
-    return JsonResponse({'message': 'Invalid request.'}, status=400)
+    return JsonResponse({'answer': 'Invalid request.'}, status=400)
 
-def analyze_image(image):
+def analyze_image(image, selected_llm, llm_query):
     image = Image.open(image).convert('RGB')
-    question = "What is the image about?"
 
     lens = Lens()
     processor = LensProcessor()
     with torch.no_grad():
-        samples = processor([image],[question])
+        samples = processor([image], [llm_query])
         lens(samples)
 
-    tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small",truncation_side = 'left',padding = True)
-    LLM_model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
+    if selected_llm == "Flan T5 Base":
+        tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
+        LLM_model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-base")
 
-    input_ids = tokenizer(samples["prompts"], return_tensors="pt").input_ids
-    outputs = LLM_model.generate(input_ids)
+        input_ids = tokenizer(samples["prompts"], return_tensors="pt").input_ids
+        outputs = LLM_model.generate(input_ids)
+        answer = tokenizer.decode(outputs[0])
+    elif selected_llm == "Flan T5 Small":
+        tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small", truncation_side='left', padding=True)
+        LLM_model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
 
-    return tokenizer.decode(outputs[0])
+        input_ids = tokenizer(samples["prompts"], return_tensors="pt").input_ids
+        outputs = LLM_model.generate(input_ids)
+        answer = tokenizer.decode(outputs[0])
+    elif selected_llm == "GPT-2":
+        generator = pipeline('text-generation', model='gpt2')
+
+        answer = generator(samples["prompts"], max_length=250, num_return_sequences=1)[0][0]['generated_text']
+        index = answer.find("Short Answer:") + len("Short Answer:")
+        answer = answer[index:].strip()
+    elif selected_llm == "BART Base":
+        tokenizer = AutoTokenizer.from_pretrained("facebook/bart-base", truncation_side='left', padding=True)
+        LLM_model = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-base")
+
+        input_ids = tokenizer(samples["prompts"], return_tensors="pt").input_ids
+        outputs = LLM_model.generate(input_ids)
+        answer = tokenizer.decode(outputs[0])
+
+    return answer

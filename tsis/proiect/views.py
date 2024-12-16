cf795c3ad34291235d03b84f19b6656830fd974f
SabinZibileanu <s_zibileanu@yahoo.com>
2024-12-16 12:13:12 +0200

    Uploaded final code

--- tsis/proiect/views.py
+++ tsis/proiect/views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 from lens import Lens, LensProcessor
 from PIL import Image
 import torch
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline, BloomTokenizerFast, BloomForCausalLM
 import re
 def multimodal_page(request):
     return render(request, 'proiect.html')
@@ -17,7 +17,7 @@ def upload_image(request):
 
         answer = analyze_image(image, selected_llm, llm_query)
         cleaned_text = re.sub(r"<.*?>", "", answer).strip()
-    # Capitalize the first letter and ensure proper punctuation
+
         result = cleaned_text[0].upper() + cleaned_text[1:]
         if not result.endswith('.'):
             result += '.'
@@ -25,7 +25,7 @@ def upload_image(request):
         return JsonResponse({
             'answer': answer
         })
-    
+
     return JsonResponse({'answer': 'Invalid request.'}, status=400)
 
 def analyze_image(image, selected_llm, llm_query):
@@ -37,32 +37,41 @@ def analyze_image(image, selected_llm, llm_query):
         samples = processor([image], [llm_query])
         lens(samples)
 
+    # If you want to run on CPU: just remove 'cuda:0'
+    # For running on GPU: if you are on PC, type: "cuda" instead of "cuda:0". Use "cuda:0" if you are on laptop
+    # For GPT - 2 you can just leave it like it is: device = 0
+
     if selected_llm == "Flan T5 Base":
         tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
         LLM_model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-base")
+        LLM_model = LLM_model.to("cuda:0")
 
-        input_ids = tokenizer(samples["prompts"], return_tensors="pt").input_ids
+        input_ids = tokenizer(samples["prompts"], return_tensors="pt").input_ids.to("cuda:0")
         outputs = LLM_model.generate(input_ids)
         answer = tokenizer.decode(outputs[0])
     elif selected_llm == "Flan T5 Small":
         tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small", truncation_side='left', padding=True)
         LLM_model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
+        LLM_model = LLM_model.to("cuda:0")
 
-        input_ids = tokenizer(samples["prompts"], return_tensors="pt").input_ids
+        input_ids = tokenizer(samples["prompts"], return_tensors="pt").input_ids.to("cuda:0")
         outputs = LLM_model.generate(input_ids)
         answer = tokenizer.decode(outputs[0])
     elif selected_llm == "GPT-2":
-        generator = pipeline('text-generation', model='gpt2')
+        generator = pipeline('text-generation', model='gpt2', device = 0)
 
         answer = generator(samples["prompts"], max_length=250, num_return_sequences=1)[0][0]['generated_text']
         index = answer.find("Short Answer:") + len("Short Answer:")
         answer = answer[index:].strip()
-    elif selected_llm == "BART Base":
-        tokenizer = AutoTokenizer.from_pretrained("facebook/bart-base", truncation_side='left', padding=True)
-        LLM_model = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-base")
 
-        input_ids = tokenizer(samples["prompts"], return_tensors="pt").input_ids
+    elif selected_llm == "BLOOM-Small":
+        tokenizer = BloomTokenizerFast.from_pretrained("bigscience/bloom-560m")
+        LLM_model = BloomForCausalLM.from_pretrained("bigscience/bloom-560m")
+        LLM_model = LLM_model.to("cuda:0")
+
+        input_ids = tokenizer(samples["prompts"], return_tensors="pt").input_ids.to("cuda:0")
         outputs = LLM_model.generate(input_ids)
-        answer = tokenizer.decode(outputs[0])
-
+        answer = tokenizer.decode(outputs[0], skip_special_tokens = True)
+        index = answer.find("Short Answer:") + len("Short Answer:")
+        answer = answer[index:].strip()
     return answer
